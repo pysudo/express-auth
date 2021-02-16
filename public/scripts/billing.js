339b5eb1788c199e536152fa9b02dc4032e56495
Siddarth Patel <siddarthpatel22@gmail.com>
2021-02-17 02:44:51 +0530

    Calculate grand total dynamically

--- public/scripts/billing.js
+++ public/scripts/billing.js
@@ -1,13 +1,14 @@
 const addRow = () => {
 
     let invoice = document.getElementById('invoice');
-    let newRow = invoice.lastElementChild.cloneNode(true);
+    let newRow = invoice.lastElementChild.previousElementSibling.cloneNode(true);
+
     newRowIndex = parseInt(newRow.id) + 1;
     newRow.id = `${newRowIndex}-item`;
     for (let i = 0; i < newRow.children.length; i++) {
         newRow.children[i].firstElementChild.value = "";
     }
-    invoice.appendChild(newRow);
+    invoice.lastElementChild.previousElementSibling.after(newRow);
 }
 
 const calcGrandPrice = (quantityId, gstId, priceId, grandPriceId) => {
@@ -26,12 +27,24 @@ const tempcalcGrandPrice = (quantityId, gstId, priceId, grandPriceId) => {
     const quantity = document.getElementById(quantityId).value;
     const gst = document.getElementById(gstId).value;
     const price = document.getElementById(priceId).value;
-
+    
     if (quantity || gst || price) {
-        
+
         const grandPrice = quantity * price * ((gst / 100) + 1);
         document.getElementById(grandPriceId).value = grandPrice.toFixed(2);
     }
+}
+
+const calcGrandTotal = () => {
+
+    a = document.getElementById('invoice');
+    let grandTotal = 0.0;
+    for (let ele = 0; ele < a.children.length - 1; ele++) {
+        b = a.children[ele].lastElementChild.firstElementChild.value;
+        grandTotal += parseFloat(b);
+    }
+
+    return parseFloat(grandTotal);
 }
 
 
@@ -42,7 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         addRow();
         let invoice = document.getElementById('invoice');
-        let newRow = invoice.lastChild;
+        let newRow = invoice.lastElementChild.previousElementSibling;
 
         for (let i = 1; i < newRow.children.length; i++) {
             let newData = newRow.children[i].firstElementChild;
@@ -56,23 +69,23 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let i = 1; i < newRow.children.length; i++) {
             let newDataId = newRow.children[i].firstElementChild.id;
             newDataIds.push(newDataId)
-            
+
         }
 
         for (let i = 1; i < newRow.children.length; i++) {
             newRow.children[i].addEventListener('keyup', () => {
-                
+
                 tempcalcGrandPrice(newDataIds[0], newDataIds[1], newDataIds[2], newDataIds[3]);
+                document.getElementById('grandTotal').lastElementChild.firstElementChild.value = calcGrandTotal();
             })
         }
     })
 
     '1-quantity 1-gst 1-price'.split(' ').forEach(function (e) {
-        
         document.getElementById(e).addEventListener('keyup', () => {
-            
+
             calcGrandPrice();
+            document.getElementById('grandTotal').lastElementChild.firstElementChild.value = calcGrandTotal();
         })
     })
 });
-

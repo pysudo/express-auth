3111f9bd9d91cb567c1f8f6552be01afe4dab7bf
Siddarth Patel <siddarthpatel22@gmail.com>
2021-03-04 02:58:14 +0530

    Modified index.js for listening server assigned port

--- index.js
+++ index.js
@@ -104,7 +104,8 @@ app.all('*', (request, response) => {
 });
 
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
 
     console.log("Serving on port 3000.");
 })
